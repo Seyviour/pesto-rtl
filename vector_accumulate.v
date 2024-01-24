6bb443a9f6a7fb231d033acbd5997572cafa50bb
Saviour <owolabisaviour@gmail.com>
2024-01-24 16:09:20 +0100

    Add module for computing vector sums with an adder cascade

--- vector_accumulate.v
+++ vector_accumulate.v
@@ -1,55 +1,55 @@
 module VectorAccumulate #(
     parameter vector_length = 7,
-    parameter data_width = 48
+    parameter data_width = 16
 ) (
     input wire clk,
-    input wire reset,
-
     input wire [vector_length*data_width-1: 0] vector_in,
-    output wire [data_width-1: 0] vector_sum
+    output wire [data_width+$clog2(vector_length)-1: 0] vector_sum
 );
 
+localparam expanded_vector_length = 2 ** ($clog2(vector_length));
+localparam length_diff = expanded_vector_length - vector_length;
+localparam left_pad_zeros = length_diff * data_width;
+
+wire [expanded_vector_length*data_width-1:0] expanded_vector_in;
+
+assign expanded_vector_in = {{length_diff{1'b0}}, vector_in}; 
 /*
 0,1,2,3,4,5,6,7,8,9,10,11,12,13
-|_| |_| |_| |_| |_| |__|   |_| 
-##|___|   |___|   |____|     |
-######|_______|        |_____|
-##############|______________|
-#############################|
+|_| |_| |_| |_| |_| |__|   |_|   
+##|___|   |___|   |____|     |  ## stage 0
+######|_______|        |_____|  ## stage 1
+##############|______________|  ## stage 2
+#############################|  ## stage 3
+
+0 -> 1 
+1 -> 3
+2 -> 7
 */
 
 // TODO: Dynamically Generate instead
 
 localparam num_stages = $clog2(vector_length);
-localparam [num_stages*8-1:0] adders_for_stage [3:0] ={0};
-genvar k;
-
-// for (k = 0; k < num_stages; k = k + 1) begin
-//     if (k == 0) begin
-//         adders_for_stage[(k+1)*8: (k*8)] = vector_length/2; 
-//     end
-// end
 
 
 generate
     begin: adder_gen
         genvar stage; 
 
-        for (stage=0; stage<num_stages; stage=stage+1) begin: stage_gen
+        for (stage=0; stage<4; stage=stage+1) begin: stage_gen
             genvar i;
-            genvar num_stage_inputs = vector_length/(2**(stage+1));
-            // num_stage_inputs =  
 
             generate
-                for (i=0; i<vector_length/2; i=i+1) begin: stage_io
-                    reg [data_width-1: 0] sum;
-                    wire [data_width-1: 0] A, B;
+                for (i=2**(stage+1)-1; i<expanded_vector_length; i=i+(2**(stage+1))) begin: stage_io
+                    reg [data_width+stage: 0] sum;
+                    wire [data_width+stage-1: 0] A, B;
                     if (stage == 0) begin
-                        assign A = vector_in[(2*i+1)*data_width-1: i*data_width];
-                        assign B = vector_in[(2*i+2)*data_width-1: (i+1)*data_width]; 
+                        assign A = expanded_vector_in[i*data_width-1: (i-1)*data_width];
+                        assign B = expanded_vector_in[(i+1)*data_width-1: i*data_width];
+
                     end else begin
-                        assign A = stage_gen[stage-1].stage_io[0].sum;
-                        assign B = stage_gen[stage-1].stage_io[1].sum; 
+                        assign A = stage_gen[stage-1].stage_io[i].sum;
+                        assign B = stage_gen[stage-1].stage_io[i-2**stage].sum; 
                     end
                     
                     always@(posedge clk) begin
@@ -57,16 +57,12 @@ generate
                     end
                 end
             endgenerate
-
         end
     end
 
 endgenerate
 
 
-
-
-
-
+assign vector_sum = adder_gen.stage_gen[num_stages-1].stage_io[expanded_vector_length-1].sum;
 
 endmodule
